52f7c01299da42d1280ce828c1475b1cceefe532
Desolatehao <zbs02177@163.com>
2025-11-09 21:43:05 +0800

    fix

--- src/components/SubNav.vue
+++ src/components/SubNav.vue
@@ -16,12 +16,13 @@ const route = useRoute()
     </button>
 
     <div mb-0 flex="~ col gap-1 sm:row sm:gap-3 wrap" text-3xl>
-      <RouterLink to="/posts" class="!border-none" :class="route.path === '/posts' ? activeStyle : inactiveStyle">
-        Blog
-      </RouterLink>
       <RouterLink to="/notes" class="!border-none" :class="route.path === '/notes' ? activeStyle : inactiveStyle">
         Notes
       </RouterLink>
+      <RouterLink to="/posts" class="!border-none" :class="route.path === '/posts' ? activeStyle : inactiveStyle">
+        Blog
+      </RouterLink>
+
     </div>
   </div>
 </template>
