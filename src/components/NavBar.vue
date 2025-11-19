34ff2c84b6d00fdda833d4246584675ac69eec72
Desolatehao <zbs02177@163.com>
2025-11-19 15:10:40 +0800

    修改顶部栏，删除测试文章

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -38,6 +38,10 @@ const { y: scroll } = useWindowScroll()
           <span class="lt-md:hidden">Notes</span>
           <div i-ri-article-line md:hidden />
         </RouterLink>
+        <RouterLink to="/media" title="Media">
+          <span class="lt-md:hidden">Media</span>
+          <div i-ri-article-line md:hidden />
+        </RouterLink>
         <RouterLink to="/photos" title="Photos">
           <div i-ri-camera-3-line />
         </RouterLink>
