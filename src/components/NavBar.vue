dfcf16d010b408f0cdbc89dd718004c4b3f13d64
desolatehao <zbs02177@163.com>
2026-07-16 16:31:07 +0800

    fix: prioritize notes in navigation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -30,12 +30,12 @@ const { y: scroll } = useWindowScroll()
     <nav class="nav">
       <div class="spacer" />
       <div class="right" print:op0>
-        <RouterLink to="/posts" title="Blog">
-          <span class="lt-md:hidden">Blog</span>
-          <div i-ri-article-line md:hidden />
-        </RouterLink>
         <RouterLink to="/notes" title="Notes">
           <span class="lt-md:hidden">Notes</span>
+          <div i-ri-article-line md:hidden />
+        </RouterLink>
+        <RouterLink to="/posts" title="Blog">
+          <span class="lt-md:hidden">Blog</span>
           <div i-ri-article-line md:hidden />
         </RouterLink>
         <!-- <RouterLink to="/media" title="Media">
